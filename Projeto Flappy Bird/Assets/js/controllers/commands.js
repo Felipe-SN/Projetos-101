d573e7f9c30534a68b4659af37f21e1a33e5c8f6
Felipe_SN <felipewfk@gmail.com>
2022-02-21 11:48:14 -0300

    reorg captura de comandos

--- Projeto Flappy Bird/Assets/js/controllers/commands.js
+++ Projeto Flappy Bird/Assets/js/controllers/commands.js
@@ -1,38 +1,39 @@
 import { telaAtiva } from '../jogo.js';
 
 const comandos = (event) => {
-  const tecla = (event) => {
-    const code = event.code;
-
-    const teclasAceitas = {
-      Space() {
-        telaAtiva.click();
-      },
-      ArrowUp() {
-        telaAtiva.click();
-      },
-    };
-
-    const fazMover = teclasAceitas[code];
-
-    if (fazMover) {
-      fazMover();
-    }
-  };
-
-  const click = () => {
-    if (telaAtiva.click) {
-      telaAtiva.click();
-    }
-  };
-
   const tipoEvento = event.type;
-  const tiposAceitos = {
-    keydown: (event) => tecla(event),
+  const code = event.code;
+  const eventosAceitos = {
+    keydown: () => tecla(event, code),
     click: () => click(),
   };
 
-  tipoEvento[tiposAceitos]();
+  eventosAceitos[tipoEvento]();
+};
+
+const tecla = (event, code) => {
+  event.preventDefault();
+  
+  const teclasAceitas = {
+    Space() {
+      telaAtiva.click();
+    },
+    ArrowUp() {
+      telaAtiva.click();
+    },
+  };
+
+  const fazMover = teclasAceitas[code];
+
+  if (fazMover) {
+    fazMover();
+  }
+};
+
+const click = () => {
+  if (telaAtiva.click) {
+    telaAtiva.click();
+  }
 };
 
 export default comandos;
